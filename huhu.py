7b439710b3367e1d3b8309d127ec05823b8799d0
Lê Quý Minh Quang <kulin2345@gmail.com>
2022-12-09 14:00:06 +0700

    Create huhu.py

---
+++ huhu.py
@@ -0,0 +1,16 @@
+#h
+class SinhVien:
+    def __init__(self,fullname,age,score):
+        self.hoten = fullname
+        self.namsinh = age
+        self.dtb = score
+    def __str__(self):
+        message = '[hoten: '+ self.hoten +'; namsinh: '+ str(self.namsinh)+'; dtb: '+ str(self.dtb) +']'
+        return message
+def main():
+    sv1 = SinhVien('Lê Quý Minh Quang',2004,10)
+    sv2 = SinhVien('Lê Hoàng Minh Quý',2004,9)
+    print(sv1)
+    print(sv2)
+if __name__ == '__main__':
+    main()
